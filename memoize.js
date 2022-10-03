bbae6fcac8155c79937149c8b3a0c6d11dfafe7b
Loganathan Sellappa <loganathan.sellappa@goto.com>
2022-10-03 21:57:14 +0200

    JS memoization function

---
+++ memoize.js
@@ -0,0 +1,32 @@
+/*
+  Simple JS memoization function
+ 
+*/
+
+
+const sum = (...args) => {
+  return args.reduce((a,b) => a+b, 0)
+}
+
+function memoize(fn) {
+  let cache = {}
+	return function() {
+    const key = [...arguments].join(':')
+    if(cache[key] === undefined) { 
+      console.log('CALL FUNCTION')
+      cache[key] = fn.call(this,...arguments)      
+    }
+    return cache[key]
+  }
+}
+
+const a = memoize(sum)
+a(1,2)
+a(1,2)
+a(1,2)
+a(1,2)
+a(1,2)
+a(1,2)
+a(4,2,3,4)
+a(4,2,3,4)
+a(4,2,3,4)
